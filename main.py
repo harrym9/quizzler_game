3bc2ca5b912ffdb8e58ef6209241e64272c2328e
Gayrat Rahmatulayev <gayratrahmatulayev8@gmail.com>
2025-02-26 23:43:53 +0300

    backend added

---
+++ main.py
@@ -0,0 +1,17 @@
+from data import question_data
+from question_model import Question
+from quiz_brain import QuizzBrain
+
+question_bank = []
+
+for question in question_data:
+    quiz = Question(question["text"], question["answer"])
+    question_bank.append(quiz)
+
+quiz = QuizzBrain(question_bank)
+
+while quiz.still_has_questions():
+    quiz.next_question()
+
+print("You've completed the quiz")
+print(f"You're final score is {quiz.user_score}/{quiz.question_number}")
