1c0e17e1e10e7a538c3ca46bd0cb242f97d7285d
Gayrat Rahmatulayev <gayratrahmatulayev8@gmail.com>
2025-02-27 00:30:15 +0300

    changes: adding quiz data from api

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from quiz_brain import QuizzBrain
 question_bank = []
 
 for question in question_data:
-    quiz = Question(question["text"], question["answer"])
+    quiz = Question(question["question"], question["correct_answer"])
     question_bank.append(quiz)
 
 quiz = QuizzBrain(question_bank)
